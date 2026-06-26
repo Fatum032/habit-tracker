825be9a09db6b3753be76ac2f8c7b4ec62d2ae95
Евгений <tashlykov091@gmail.com>
2026-06-26 22:51:00 +0300

    Fix FormEvent type imports for Vercel build

--- src/pages/PrayerNewPage.tsx
+++ src/pages/PrayerNewPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
+import type { FormEvent } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import * as db from '../lib/db'
 import { useAuth } from '../context/AuthContext'
